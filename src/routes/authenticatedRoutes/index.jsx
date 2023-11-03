333386158535cb17b464a3f02876335da3aa0a42
Dayo <abegunde.michael2@gmail.com>
2023-11-03 16:35:14 +0100

    add investments page

--- src/routes/authenticatedRoutes/index.jsx
+++ src/routes/authenticatedRoutes/index.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Dashboard from '../../components/AuthenticatedApp/views/Dashboard';
 import LPOs from '../../components/AuthenticatedApp/views/Lpos/LPOs';
 import LPODetails from '../../components/AuthenticatedApp/views/Lpos/LPODetails';
+import Investments from '../../components/AuthenticatedApp/views/Investments/Investments';
 
 
 function AuthenticatedRoutes() {
@@ -11,6 +12,7 @@ function AuthenticatedRoutes() {
             <Route exact path="/" element={<Dashboard />} /> 
             <Route path="/lpos" element={<LPOs />} />
             <Route path="/lpos-details" element={<LPODetails />} />
+            <Route path ="/investments" element={<Investments />} />
         </Routes>
     )
 }
