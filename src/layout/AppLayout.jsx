96e8ff35ccbe8521b840c99236db170158b389d9
Dayo <abegunde.michael2@gmail.com>
2024-05-13 08:57:57 +0100

    code cleanup

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import CharizHomesLogo from "../assets/la-icons/homes-logo.svg"
 
 function AppLayout({ children }) {
+    const navigate = useNavigate()
     const location = useLocation();
     const navLinks = [
         { name: 'Home', link: '/' },
@@ -13,9 +14,12 @@ function AppLayout({ children }) {
         { name: 'Our App', link: '/app' },
         
     ];
+    const goTo = (link) => {
+        navigate(link)
+    }
     return (
-        <div className='w-full h-full flex flex-col font-comfortaa z-100'>
-            <div className='w-full px-10 md:px-20 flex fixed bg-[#fff]'>
+        <div className='w-full h-full flex flex-col font-comfortaa'>
+            <div className='w-full px-10 md:px-20 flex fixed bg-[#fff] z-50'> {/* Ensure this navbar is above other content */}
                 <div className='w-1/5'>
                     <img src={CharizHomesLogo} />
                 </div>
@@ -24,7 +28,7 @@ function AppLayout({ children }) {
                         {navLinks.map((navItem, index) => (
                             <div
                                 key={index}
-                                className={` text-md ${location.pathname === navItem.link ? 'nav-item active' : 'nav-item'}`}
+                                className={` text-sm ${location.pathname === navItem.link ? 'nav-item active' : 'nav-item'}`}
                             >
                                 <a href={navItem.link} style={{ color: location.pathname === navItem.link ? 'text-[#F29254]' : 'text-[#000]' }}>
                                     {navItem.name}
@@ -32,24 +36,21 @@ function AppLayout({ children }) {
                             </div>
                         ))}
                     </div>
-                    <div className='flex gap-4 justify-end'>
-                        <button className='rounded-3xl border border-2 border-[#F29254] font-semibold text-lg py-2 px-8 text-[#F29254] flex justify-center items-center'>
+                    <div className='flex gap-4 justify-end text-sm'>
+                        <button className='rounded-3xl border border-2 border-[#F29254] font-semibold text-lg py-2 px-8 text-[#F29254] flex justify-center items-center' onClick={() => goTo("/signup")}>
                             Sign up
                         </button>
-                        <button className='floating-button rounded-3xl bg-[#F29254] font-semibold text-lg py-2 px-8 text-[#fff] flex justify-center items-center'>
+                        <button className='floating-button rounded-3xl bg-[#F29254] font-semibold text-lg py-2 px-8 text-[#fff] flex justify-center items-center' onClick={() => goTo("/login")}>
                            Login
                         </button>
-
                     </div>
                 </div>
             </div>
-            <div className='flex  flex-col w-full'>
-            {children}
+            <div className='flex flex-col w-full'>
+                {children}
             </div>
-            
         </div>
-
     )
 }
 
-export default AppLayout
+export default AppLayout;
